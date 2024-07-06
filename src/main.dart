eae27a6d0336809870c0de304a4f92cdfda5a9f7
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-06 15:58:16 +0700

    Dart_Learning_Package

--- src/main.dart
+++ src/main.dart
@@ -1,5 +1,14 @@
 void main(){
   String name = "Ezzie";
   String name2 = "Ezekiel";
+  int n = 2;
+  double n1 = 10.0;
+  num numAny = 2.00;// can set both int and double
   print(name);
+  print(n);
+  print(n1);
+  print(numAny);
+  print(n.isEven);
+  print(n1.sign);
+
 }
